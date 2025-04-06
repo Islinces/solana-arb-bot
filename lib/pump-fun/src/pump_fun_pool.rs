e8b2e80196a080a46d67c2eff5576b834fd90f37
于鹏22074 <86756559+Islince@users.noreply.github.com>
2025-04-06 18:46:53 +0800

    pump fun test

--- lib/pump-fun/src/pump_fun_pool.rs
+++ lib/pump-fun/src/pump_fun_pool.rs
@@ -78,12 +78,11 @@ impl Pool for PumpFunPool {
                 .mul(effective_amount)
                 .div(quote_vault.add(effective_amount))
         };
-        let amount_out = amount_out.try_into().unwrap_or_else(|_| {
+        println!("total_fee: {}", total_fee);
+        Some(amount_out.try_into().unwrap_or_else(|_| {
             eprintln!("amount_out is too large");
             u64::MIN
-        });
-        println!("total_fee: {}", total_fee);
-        Some(amount_out)
+        }))
     }
 
     fn clone_box(&self) -> Box<dyn Pool> {
