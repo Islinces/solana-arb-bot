269fa9255b2942da664b434cb9e29b4c22538b5b
于鹏22074 <86756559+Islince@users.noreply.github.com>
2025-05-29 16:20:49 +0800

    password

--- bin/router/src/metadata.rs
+++ bin/router/src/metadata.rs
@@ -28,8 +28,7 @@ pub(crate) async fn init_metadata(
 ) -> anyhow::Result<()> {
     loop {
         println!("请输入密码：");
-        // let input_password = read_password().expect("读取密码失败");
-        let input_password = "test_password123".to_string();
+        let input_password = read_password().expect("读取密码失败");
         let keypair_vault = KeypairVault::load(PathBuf::from_str(&keypair_path)?)?;
         if let Ok(k) = keypair_vault.decrypt(&input_password) {
             let wallet = &k.pubkey();
